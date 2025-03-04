e15ec4750624e229c3d2d3452c2f45f43d4c0194
tuchan2002 <trananhtu171002@gmail.com>
2025-03-04 10:52:07 +0700

    push

--- classifier.py
+++ classifier.py
@@ -1,13 +1,24 @@
+import re
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from ydata_profiling import ProfileReport
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
 import numpy as np
+
 
 data = pd.read_csv("data/train.csv")
 x_test = pd.read_csv("data/test.csv", index_col="PassengerId")
+
+# profile = ProfileReport(data, title="Titanic", explorative=True)
+# profile.to_file("titanic.html")
 
 target = "Survived"
 
@@ -15,6 +26,20 @@ data.set_index("PassengerId", inplace=True)
 family_size_labels = ["Single", "Small", "Medium", "Large"]
 data["Family_Size"] = data["SibSp"] + data["Parch"] + 1
 data["Family_Size"] = pd.cut(data["Family_Size"], bins=[0,1,4,6,20], labels=family_size_labels)
+
+def filter_name(name):
+  match = re.search(r"\b([A-Z][a-z]+)\b(?=\.)", name)
+  return match.group() if match else name
+
+def group_title(title):
+  if title in ["Mr", "Miss", "Mrs", "Master"]:
+    return title
+  elif title == "Ms":
+    return "Miss"
+  else:
+    return "Others"
+  
+data["Title"] = data["Name"].apply(filter_name).apply(group_title)
 
 x = data.drop(target, axis=1)
 y = data[target]
@@ -38,11 +63,28 @@ ord_transformer = Pipeline(steps=[
 
 preprocessor = ColumnTransformer(transformers=[
   ("num_feature", num_transformer, ["Age", "Fare"]),
-  ("nom_feature", nom_transformer, ["Cabin", "Embarked", "Ticket"]),
+  ("nom_feature", nom_transformer, ["Cabin", "Embarked", "Ticket", "Title"]),
   ("ord_feature", ord_transformer, ["Sex", "Family_Size"]),
 ])
 
-x_train = preprocessor.fit_transform(x_train)
+cls = Pipeline(steps=[
+  ("preprocessor", preprocessor),
+  # ("model", SVC()),
+  ("model", RandomForestClassifier()),
+])
 
-print(x_train)
+params = {
+  "model__n_estimators": [100, 200, 300],
+  "model__criterion": ["gini", "entropy", "log_loss"],
+  "model__max_depth": [None, 2, 5],
+}
+
+grid_search = GridSearchCV(estimator=cls, param_grid=params, cv=4, scoring="accuracy", verbose=2, n_jobs=-1)
+grid_search.fit(x_train, y_train)
+y_predicted = grid_search.predict(x_valid)
+
+print(classification_report(y_valid, y_predicted))
+print(grid_search.best_score_)
+print(grid_search.best_params_)
+
 
