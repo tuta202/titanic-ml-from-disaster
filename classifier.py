6df459dbb40e5e8374b48e149f4c56254a7535f4
tuchan2002 <trananhtu171002@gmail.com>
2025-03-04 12:06:56 +0700

    done 1

--- classifier.py
+++ classifier.py
@@ -23,9 +23,12 @@ x_test = pd.read_csv("data/test.csv", index_col="PassengerId")
 target = "Survived"
 
 data.set_index("PassengerId", inplace=True)
+
 family_size_labels = ["Single", "Small", "Medium", "Large"]
 data["Family_Size"] = data["SibSp"] + data["Parch"] + 1
 data["Family_Size"] = pd.cut(data["Family_Size"], bins=[0,1,4,6,20], labels=family_size_labels)
+x_test["Family_Size"] = x_test["SibSp"] + x_test["Parch"] + 1
+x_test["Family_Size"] = pd.cut(x_test["Family_Size"], bins=[0,1,4,6,20], labels=family_size_labels)
 
 def filter_name(name):
   match = re.search(r"\b([A-Z][a-z]+)\b(?=\.)", name)
@@ -40,6 +43,7 @@ def group_title(title):
     return "Others"
   
 data["Title"] = data["Name"].apply(filter_name).apply(group_title)
+x_test["Title"] = x_test["Name"].apply(filter_name).apply(group_title)
 
 x = data.drop(target, axis=1)
 y = data[target]
@@ -56,15 +60,16 @@ nom_transformer = Pipeline(steps=[
 ])
 
 gender_values = x_train["Sex"].unique()
+pclass_values = x_train["Pclass"].unique()
 ord_transformer = Pipeline(steps=[
   ("imputer", SimpleImputer(strategy="most_frequent")),
-  ("encoder", OrdinalEncoder(categories=[gender_values, family_size_labels])),
+  ("encoder", OrdinalEncoder(categories=[gender_values, pclass_values, family_size_labels])),
 ])
 
 preprocessor = ColumnTransformer(transformers=[
   ("num_feature", num_transformer, ["Age", "Fare"]),
   ("nom_feature", nom_transformer, ["Cabin", "Embarked", "Ticket", "Title"]),
-  ("ord_feature", ord_transformer, ["Sex", "Family_Size"]),
+  ("ord_feature", ord_transformer, ["Sex", "Pclass", "Family_Size"]),
 ])
 
 cls = Pipeline(steps=[
@@ -81,10 +86,16 @@ params = {
 
 grid_search = GridSearchCV(estimator=cls, param_grid=params, cv=4, scoring="accuracy", verbose=2, n_jobs=-1)
 grid_search.fit(x_train, y_train)
-y_predicted = grid_search.predict(x_valid)
+y_valid_predicted = grid_search.predict(x_valid)
 
-print(classification_report(y_valid, y_predicted))
+print(classification_report(y_valid, y_valid_predicted))
 print(grid_search.best_score_)
 print(grid_search.best_params_)
 
+y_test_predicted = grid_search.predict(x_test)
 
+submission = pd.DataFrame({
+  "PassengerId": x_test.index,
+  "Survived": y_test_predicted
+})
+submission.to_csv("data/gender_submission.csv", index=False)
